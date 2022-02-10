e2022788e36e9fc57b3105aaee73b4eb6daf4c3b
BoBrIkkz <78279032+BoBrIkkz@users.noreply.github.com>
2022-02-10 20:41:59 +0300

    complete lesson 23

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import one from './modules/one'
-import two from './modules/two'
+import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
+import link from './modules/link'
+import validation from './modules/validation'
+import tabs from './modules/tabs'
+import slider from './modules/slider'
+timer('11 february 2023')
+menu()
+modal()
+link()
+validation()
+tabs()
+slider()
