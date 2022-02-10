aaca1382082cdfed27c9d24af3b3313a393f65ed
BoBrIkkz <78279032+BoBrIkkz@users.noreply.github.com>
2022-02-10 10:45:10 +0300

    complete lesson 21

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import one from './modules/one'
-import two from './modules/two'
+import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
+import link from './modules/link'
+import validation from './modules/validation'
+timer('11 february 2023')
+menu()
+modal()
+link()
+validation()
